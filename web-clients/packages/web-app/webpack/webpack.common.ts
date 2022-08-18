f57ec621ded919f684bafb8169988f1e350e3400
tunguyennnnn <conghoa522@gmail.com>
2022-08-18 18:06:00 -0400

    Fixed webpack

---
+++ web-clients/packages/web-app/webpack/webpack.common.ts
@@ -0,0 +1,44 @@
+import configPath from './config.path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  entry: configPath.entryPath,
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: configPath.templatePath,
+    }),
+  ],
+};
---
+++ web-clients/packages/web-app/webpack/webpack.prod.ts
@@ -0,0 +1,29 @@
+import { Configuration } from 'webpack';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import ESLintPlugin from 'eslint-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+
+import configPath from './config.path';
+
+const config: Configuration = {
+  mode: 'production',
+  output: {
+    path: configPath.outputPath,
+    filename: '[name].[contenthash].js',
+    publicPath: '',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+    }),
+    new ESLintPlugin({
+      extensions: ['js', 'jsx', 'ts', 'tsx'],
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
+
+export default config;

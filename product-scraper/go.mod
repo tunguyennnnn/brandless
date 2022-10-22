b53072a2ed886543df6ae86d928fda0d99b3eba7
tunguyennnnn <conghoa522@gmail.com>
2022-10-22 12:01:39 -0400

    Added scraping server

--- product-scraper/go.mod
+++ product-scraper/go.mod
@@ -4,5 +4,20 @@ go 1.18
 
 require (
 	google.golang.org/grpc v1.49.0
-	google.golang.org/protobuf v1.27.1
+	google.golang.org/protobuf v1.28.1
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/antchfx/htmlquery v1.2.5 // indirect
+	github.com/antchfx/xmlquery v1.3.12 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	golang.org/x/net v0.0.0-20220909164309-bea034e7d591 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 )
--- product-scraper/server.go
+++ product-scraper/server.go
@@ -1,34 +1,11 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net"
-	adminapis "product-scraper/adminapis"
 
-	"google.golang.org/grpc"
+	grpcServer "product-scraper/grpc"
 )
-
-type grpcServer struct {
-	adminapis.UnimplementedAdminAPIServiceServer
-}
-
-func NewRPCServer() *grpc.Server {
-	srv := grpcServer{}
-
-	gsrv := grpc.NewServer()
-	adminapis.RegisterAdminAPIServiceServer(gsrv, &srv)
-	return gsrv
-}
-
-func (lss *grpcServer) PullProducts(ctx context.Context, req *adminapis.PullProductsRequest) (*adminapis.PullProductsResponse, error) {
-
-	res := adminapis.PullProductsResponse{
-		IsDone:     false,
-		Percentage: 10,
-	}
-	return &res, nil
-}
 
 func main() {
 	conn, err := net.Listen("tcp", ":4001")
@@ -37,7 +14,7 @@ func main() {
 		log.Fatal("tcp connection error: ", err.Error())
 	}
 
-	grpcServer := NewRPCServer()
+	grpcServer := grpcServer.NewRPCServer()
 
 	log.Printf("connect to %s", "4001")
 
